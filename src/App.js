f78c8b2d80fb7f7133b97a4090e1f7d0cfb8aa17
balakrishnansky <balakrishnan.sky@gmail.com>
2020-11-21 15:38:12 -0800

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import ContactList from "./components/ContactList";
 import "./style.css";
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
+      <ContactList />
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
---
+++ src/components/ContactList.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default function ContactList() {
+  return (
+    <div>
+      <h1>Contact List </h1>
+    </div>
+  );
+}
