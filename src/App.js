ab64f0bcec45579648c581c24b29651bb0cf0991
balakrishnansky <balakrishnan.sky@gmail.com>
2020-11-21 19:38:02 -0800

    3 files modified

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ import datasource from "./components/datasource";
 import "./style.css";
 
 export default function App() {
-  console.log(datasource);
-  const readData = datasource.map(datasrc => {
-    return <ContactList name={datasrc.data.name} />;
+  console.log(datasource.data);
+  //const getArray = [...datasource.data];
+  const readData = datasource.data.map(datasource => {
+    // return datasource.data.name;
+    return (
+      <ContactList
+        name={datasource.name}
+        description={datasource.description}
+      />
+    );
   });
 
   return (
     <div>
+      {readData}
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -5,6 +5,7 @@ export default function ContactList(props) {
   return (
     <div>
       <h1>{props.name}</h1>
+      <p>{props.description}</p>
     </div>
   );
 }
